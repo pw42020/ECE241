bba8d14f8014513d599d3f1f6e6fcff88e53d2fa
pw42020 <pw4.2020@gmail.com>
2021-09-02 12:11:44 -0400

    Added default things for class

---
+++ Lectures/9.2.21.py
@@ -0,0 +1,52 @@
+'''
+Lecture 1: Introduction to Data Scructures and Algorithms
+    What is an algorithm?
+        Well-defined computational procedure
+        Takes set of values as input and produces set of values as output
+        Tool to solve well defined computational problems
+    A Data-Centric View of the World
+        Data is import in many disciplines
+            i.e. COVID data across disciplines and images, network traffic, etc.
+        Past ECE focus: Processing
+        New ECE focus: Data
+            Data at fcore; data centers, data security, data science, etc.
+        Need structured approach to handling data
+    What kind of problems are solved by algorithms?
+        Human Genome Project
+        Internet obviously
+        Commercial enterprises
+    Internet
+        Routing algorithms that find shortest path and adapt to changes
+        Search engines that find pages with particular information
+        Encrypted information
+        Queue Management -- internet searches and links go into a queue so you can pull them back out
+    Why Data Structures?
+        Way to store and organize data
+        Facilitate access and modificaiton of data
+        Different data structures have strengths and weaknesses
+        Better suited for a specific algorithm than others
+    Data Structures and Algorithms
+        Data Structures
+            Representation and organization of data
+    Big Data
+        Extremely Large volumes of data
+        Can be computationally analyzed, i.e. Internet of Things and Social Media
+    Artificial Intelligence
+        Often trained on big data sets
+            Image recognition
+            Voice recognition
+        Relies on a large set of algorithm, varying in complexity
+            Linear regression, stats, and probability
+    Complexity Problems
+        Data sets can be very large
+        Operations may take a lot of time or memory
+            Efficient implemenations can make a big difference
+        Examples of large data sets and sizes?
+            Million song data set from Columbia
+                Data set of songs: title, artist, recording years, etc.
+            How to find out who recorded the most songs, which song has been covered the most, etc?
+    Properties of Data Structures and Algorithms
+    
+
+
+'''
